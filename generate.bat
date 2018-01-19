df984b1130cd963faa9bef5118c28d3c87c7dcc5
Orkun Uçan <8121333+orkunucan@users.noreply.github.com>
2018-01-19 13:12:37 +0200

    Add files via upload

---
+++ generate.bat
@@ -0,0 +1,2 @@
+ffmpeg -i SampleAudio_0.4mb.mp3 -filter_complex "showwavespic=s=1920x200:colors=#ff9500" -frames:v 1 SampleAudio_0.4mb.png
+ffmpeg -i SampleAudio_0.7mb.mp3 -filter_complex "showwavespic=s=1920x200:colors=#f7f7f7" -frames:v 1 SampleAudio_0.7mb.png
